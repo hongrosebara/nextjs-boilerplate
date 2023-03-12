60c18a2520503b73ae16973c54decea4bd7b00e0
Hong Le <hongrosebara@gmail.com>
2023-03-12 15:13:38 +0700

    added screen to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,12 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
     extend: {
       colors: {
         primary: 'var(--clr-primary)',
@@ -27,7 +33,23 @@ module.exports = {
         'hover-2': 'var(--clr-hover-2)',
         'hover-3': 'var(--clr-hover-3)',
         light: 'var(--clr-light)'
-      }
+      },
+      fontSize: {
+        xs: 'var(--fs-200)',
+        sm: 'var(--fs-300)',
+        base: 'var(--fs-400)',
+        xl: 'var(--fs-500)',
+        '2xl': 'var(--fs-600)',
+        '3xl': 'var(--fs-700)',
+        '4xl': 'var(--fs-800)',
+        '5xl': 'var(--fs-900)',
+      },
+      fontFamily: {
+        'heading': ['Poppins'],
+        'subheading': ['Raleway'],
+        'serif': ['Roboto Slab'],
+        'body': ['Roboto']
+      },
     },
   },
   plugins: [],
